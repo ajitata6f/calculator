7a234d543e1170be650ae64a678b24ca5886ef16
Adamu Abdulaziz <ajitata6f@gmail.com>
2024-09-17 17:38:37 +0100

    fix constant issue

--- src/main/java/com/ajitata6f/calculator/CalculatorApplication.java
+++ src/main/java/com/ajitata6f/calculator/CalculatorApplication.java
@@ -3,11 +3,16 @@ package com.ajitata6f.calculator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 @SpringBootApplication
 public class CalculatorApplication {
-    private static final String constant = "constant";
+    private static final String CONSTANT = "constant";
 
     public static void main(String[] args) {
+        Logger.getLogger(CalculatorApplication.class.getName()).log(Level.INFO, CONSTANT);
+
         SpringApplication.run(CalculatorApplication.class, args);
     }
 
